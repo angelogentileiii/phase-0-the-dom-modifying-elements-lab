aa4a6015e64999662a97ccf6e91cb64a38c1863d
Angelo Gentile III <angelo.john.gentile@gmail.com>
2023-08-31 09:32:59 -0400

    completed DOM Elements Lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+document.querySelector('main#main').remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.setAttribute("id","victory");
+
+newHeader.innerHTML = "Angelo is the champion";
+
+document.body.append(newHeader);
