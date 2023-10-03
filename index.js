82a246d8977a028b16d32673a25ba46b061597cd
Angelo Gentile III <angelo.john.gentile@gmail.com>
2023-10-03 13:57:46 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Write your code here!
 
+/*
 document.querySelector('main#main').remove();
 
 const newHeader = document.createElement('h1');
@@ -9,3 +10,18 @@ newHeader.setAttribute("id","victory");
 newHeader.innerHTML = "Angelo is the champion";
 
 document.body.append(newHeader);
+
+*/
+
+document.querySelector('main#main').remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.setAttribute('id','victory');
+
+newHeader.textContent = "Angelo is the champion.";
+
+document.body.append(newHeader);
+
+
+
